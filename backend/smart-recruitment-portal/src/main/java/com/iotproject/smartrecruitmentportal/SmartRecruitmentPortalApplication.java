28793341091b8a91ea2e8536781ea68cc9d338cd
Kumar-Kale <premakale092@gmail.com>
2026-07-09 19:15:19 +0530

    Initial project setup

---
+++ backend/smart-recruitment-portal/src/main/java/com/iotproject/smartrecruitmentportal/SmartRecruitmentPortalApplication.java
@@ -0,0 +1,13 @@
+package com.iotproject.smartrecruitmentportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRecruitmentPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRecruitmentPortalApplication.class, args);
+	}
+
+}
